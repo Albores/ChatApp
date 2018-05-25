92ace6567e0b38eb367cef15b7e8d3d74e163ba6
Albores López Jorge Daniel <daknst@gmail.com>
2018-05-24 21:48:29 -0500

    cosa antes de dormir

--- app/src/main/java/com/example/fiuady/chatapp/CreateChat.java
+++ app/src/main/java/com/example/fiuady/chatapp/CreateChat.java
@@ -7,15 +7,19 @@ import android.widget.TextView;
 
 public class CreateChat extends AppCompatActivity {
     private TextView contact;
+    private TextView participants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_chat);
         contact = findViewById(R.id.actualcontact_tv);
+        participants = findViewById(R.id.participantschat_text);
         Intent intent = getIntent();
         String contactusername = intent.getStringExtra(ChatActivity.EXTRA_HIDED_ID);
         contact.setText(contactusername);
+        String parti = contactusername;
+        participants.setText(parti);
 
     }
 }
